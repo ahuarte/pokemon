88bf254efcc75813fe931624c4872d80535c946a
javiab10 <160053466+javiab10@users.noreply.github.com>
2025-05-19 11:54:27 +0200

    Método submit para formulario de jugador + Lista de jugadores

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CardFormComponent } from "./forms/card-form/card-form.component";
 import { CardListComponent } from "./card-list/card-list.component";
+import { PlayerListComponent } from "./player-list/player-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CardFormComponent, CardListComponent],
+  imports: [RouterOutlet, NavbarComponent, CardFormComponent, CardListComponent, PlayerListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
